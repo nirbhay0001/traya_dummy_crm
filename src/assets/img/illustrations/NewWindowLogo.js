371d831878d0b112c2899a07649289f8c62f914d
dsouzaprasan <Dsouzaprasan@trayahealth.com>
2023-05-16 10:47:49 +0530

    Added image and icon in component formate

---
+++ src/assets/img/illustrations/NewWindowLogo.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function NewWindowLogo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="15"
+      enableBackground="new 0 0 512 512"
+      viewBox="0 0 24 24"
+    >
+      <g data-name="25. Open">
+        <path
+          d="M20 10v9a1 1 0 01-1 1H5a1 1 0 01-1-1V5a1 1 0 011-1h9a1 1 0 000-2H5a3 3 0 00-3 3v14a3 3 0 003 3h14a3 3 0 003-3v-9a1 1 0 00-2 0z"
+          data-original="#000000"
+        ></path>
+        <path
+          d="M21 2h-3a1 1 0 000 2h.59l-7.3 7.29a1 1 0 001.42 1.42L20 5.41V6a1 1 0 002 0V3a1 1 0 00-1-1z"
+          data-original="#000000"
+        ></path>
+      </g>
+    </svg>
+  );
+}
+
+export default NewWindowLogo;
